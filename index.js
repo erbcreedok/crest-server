48758b2be464abe31ee40029fd043b14d3266b72
Yerbol Syzdyk <yerbol_syzdyk@epam.com>
2024-03-21 02:12:32 +0500

    change server configuration

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const session = require('express-session')
 const port = process.env.PORT || 3333
 const route = require('./app/routes')
 
-app.use(cors({origin: JSON.parse(process.env.CORS_ALLOWED || '["http://192.168.0.17:8080", "http://localhost:8080"]'), credentials: true}))
+app.use(cors({origin: JSON.parse(process.env.CORS_ALLOWED || '["http://192.168.1.69:8080", "http://localhost:8080", "https://3340-147-30-52-154.ngrok-free.app", "164.92.206.196:8080"]'), credentials: true}))
 app.use(session({
   secret: 'crest-online',
   resave: true,
